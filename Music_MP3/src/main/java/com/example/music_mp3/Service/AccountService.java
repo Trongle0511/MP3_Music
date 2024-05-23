e07e7ddc39c5350cfca25db3d37b42c9c5ceb6f7
hung21904 <nguyenhung21904@gmail.com>
2024-05-23 22:16:48 +0700

    Login

--- Music_MP3/src/main/java/com/example/music_mp3/Service/AccountService.java
+++ Music_MP3/src/main/java/com/example/music_mp3/Service/AccountService.java
@@ -1,7 +1,30 @@
-//package com.example.music_mp3.Service;
-//
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public interface AccountService {
-//}
+package com.example.music_mp3.Service;
+
+import com.example.music_mp3.Entity.Account;
+import com.example.music_mp3.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public boolean authenticateUser(String email, String password) {
+        Account account = accountRepository.findByEmail(email);
+        if (account == null || password == null) {
+            return false;
+        }
+        return BCrypt.checkpw(password, account.getHashedPassword());
+    }
+
+    public boolean isAdmin(String email) {
+        Account account = accountRepository.findByEmail(email);
+        return account != null && account.is_admin();
+    }
+}
+
+
+
